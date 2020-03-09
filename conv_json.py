680933d57224f4e1b20754134590b8ee832f0b0e
Anshuman Anand <anshuman_anand@trisysit.com>
2020-03-09 18:01:59 +0530

    new changes

--- conv_json.py
+++ conv_json.py
@@ -2,11 +2,15 @@ from python_json_config import ConfigBuilder
 builder=ConfigBuilder()
 
 config=builder.parse_config('json_format.json')
-print(config.mysql.host)
-print("1) import config by typing 'config=conv_json.config'\n")
-access=tuple(config)
-print("2) Type the following command to access the element\n")
+access=list(config)
+k=0
 for i in access:
-    print('config.'+i)
+
+   c=i.split('.')[1]
+   a="print"+"("+"config."+i+")"
+   print(i.split('.')[0]+" "+c+"=")
+   exec(a)
+   print('\n')
+
 
    
